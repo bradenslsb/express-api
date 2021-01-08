4083b235930d112c8119ffa7456e8b2c9a18870d
bradenslsb <bbir1199@gmail.com>
2021-01-08 11:57:44 -0700

    Set up the PATCH route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const todoModel = require("../models/todoModel");
 const router = express.Router();
 const Todos = require("../models/todoModel");
 // GET
@@ -29,4 +30,24 @@ router.post("/todo", (req, res) => {
 			res.status(400).json({ message: "unable to post", errors: `${err}` });
 		});
 });
+
+//PATCH
+router.patch("/todo/:id", (req, res) => {
+  Todos.findById(req.params.id, {}, {}, (error, todo) => {
+    if(err){
+      res.status(404).json({ message: " patch problem", errors: `${err}` })
+    }else {
+      todo.done = req.body.done
+
+      todo 
+        .save()
+        .then(todo => {
+          res.status(204).json(todo)
+        })
+        .catch(err => {
+          res.status(400).json({ message: "unable to update", errors: `${err}` })
+        })
+    }
+  })
+})
 module.exports = router;
